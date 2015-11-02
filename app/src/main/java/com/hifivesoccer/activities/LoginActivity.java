57db7640633ea2b916e0d1c466aed6855ab4de4d
Jorand Le Pape <jorand.lepape@gmail.com>
2015-11-02 13:21:27 +0100

    Subscribe add user name and add link in login

--- app/src/main/java/com/hifivesoccer/activities/LoginActivity.java
+++ app/src/main/java/com/hifivesoccer/activities/LoginActivity.java
@@ -47,28 +47,6 @@ public class LoginActivity extends AppActivity {
 //        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void toggleVisibility(View view) {
 
         EditText editText = (EditText) findViewById(R.id.act_login_password);
@@ -107,5 +85,6 @@ public class LoginActivity extends AppActivity {
 
         Intent intent = new Intent(this, SubscribeActivity.class);
         startActivity(intent);
+        finish();
     }
 }
