8bd7ac2b7906e5a279320b3d2ccf27f17780d170
Jorand Le Pape <jorand.lepape@gmail.com>
2015-11-04 13:13:14 +0100

    Add New game button FAB

--- app/src/main/java/com/hifivesoccer/activities/MainActivity.java
+++ app/src/main/java/com/hifivesoccer/activities/MainActivity.java
@@ -2,11 +2,15 @@ package com.hifivesoccer.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.hifivesoccer.R;
@@ -19,6 +23,7 @@ public class MainActivity extends AppActivity {
     private final Context context = this;
 
     Toolbar toolbar;
+    private CoordinatorLayout coordinatorLayout;
     ViewPager pager;
     ViewPagerAdapter adapter;
     SlidingTabLayout tabs;
@@ -30,11 +35,10 @@ public class MainActivity extends AppActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Creating The Toolbar and setting it as the Toolbar for the activity
-
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
 
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
         adapter =  new ViewPagerAdapter(this, getSupportFragmentManager(),Titles,Numboftabs);
@@ -59,10 +63,19 @@ public class MainActivity extends AppActivity {
         // Setting the ViewPager For the SlidingTabsLayout
         tabs.setViewPager(pager);
 
+        // FAB
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Snackbar.make(coordinatorLayout, "FAB Clicked", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
         if(MySelf.getSelf() != null){
             //TextView welcome = (TextView) findViewById(R.id.act_main_welcome);
-            //String username = MySelf.getSelf().getProfile().getUsername();
+            String username = MySelf.getSelf().getProfile().getUsername();
             //welcome.setText("Bienvenue " + username);
+            Snackbar.make(coordinatorLayout, "Bienvenue " + username + " !", Snackbar.LENGTH_SHORT).show();
         }
 
     }
